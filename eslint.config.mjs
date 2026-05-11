2ca619fc6aa16b356875c8f2c17d08e901f135e1
LorranOliveiraD <lorranmikaelson@gmail.com>
2026-05-11 20:55:35 -0300

    fix: instalar eslint e configurar flat config para o monorepo

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import js from "@eslint/js";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+      },
+      globals: {
+        process: "readonly",
+        require: "readonly",
+        module: "readonly",
+        __dirname: "readonly",
+        console: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "no-unused-vars": "off",
+    },
+  },
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/*.js",
+      "**/generated/**",
+    ],
+  },
+];
